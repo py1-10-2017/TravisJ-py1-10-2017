d3d47c39f9173a03c8fa67916d5de4f8cb183d4e
MaddysDaddy <cap.zing@gmail.com>
2017-10-09 19:34:00 -0700

    Last update
    Improving usability and continuity.

--- FlaskFundamentals/great_number_game/server.py
+++ FlaskFundamentals/great_number_game/server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, session, request
 app = Flask(__name__)
 
 
-app.secret_key = 'lskdjf;lajfr'
+app.secret_key = 'lskdjf;lajf'
 
 import random
 
@@ -12,16 +12,26 @@ import random
 def main():
     if 'winning_number' not in session:
         session['winning_number'] = random.randrange(0, 101)
-        session['response'] = "Take a Guess"
         print session['winning_number'], "- is the winning number"
 
-    return render_template('index.html', response=session['response'])
+    #session['response'] = "Take a Guess"
+    if 'response' not in session and 'winner' not in session:
+        print "case 1"
+        return render_template('index.html')
+    elif 'winner' not in session:
+        print "case 2"
+        return render_template('index.html', response=session['response'])
+    else:
+        print "case 3"
+        return render_template('index.html', response=session['response'], winner=session['winner'])
 
 
 @app.route('/guess', methods=['POST'])
 def guess():
     guess = request.form['guess']
     winning_number = session['winning_number']
+    if 'winner' in session:
+        session.pop('winner')
 
     if int(guess) > int(winning_number):
         print "Too high."
@@ -34,13 +44,14 @@ def guess():
     else:
         print winning_number, " is correct! You win!"
         session['response'] = str(winning_number) + " - You win!"
+        session['winner'] = "A new number has been selected. Start guessing to play again!"
         session.pop('winning_number')
         return redirect('/')
 
 
 @app.route('/reset', methods=['POST'])
 def reset():
-
+    session.pop('winning_number')
     return redirect('/')
 
 
