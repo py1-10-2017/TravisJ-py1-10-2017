8954a3372d7dff993c54feb9ae2b7e398a9dae06
MaddysDaddy <cap.zing@gmail.com>
2017-11-04 15:42:35 -0700

    Updating user login email field

--- Django/userLogin/apps/user_login/models.py
+++ Django/userLogin/apps/user_login/models.py
@@ -9,7 +9,7 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
     age = models.IntegerField()
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now=True)
